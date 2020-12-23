8ea195611147c536a2805f83165654dc5a968dad
Carlos Monge <cmonge@jackdevelopers.com>
2020-12-22 19:05:38 -0600

    Add new module

---
+++ skipMH_module/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+	'name': 'UltraFit',
+	'version': '12.0.2.0.0',
+	'author': 'Sefinteccr S.A.',
+    'license': 'OPL-1',
+	'website': 'http://www.sefinteccr.com',
+	'category': 'Account',
+	'description':
+		'''
+		Skip MH
+		''',
+	'depends': ['cr_electronic_invoice'],
+	'data': [
+		'views/res_partner_views.xml',
+	],
+	'installable': True,
+}
