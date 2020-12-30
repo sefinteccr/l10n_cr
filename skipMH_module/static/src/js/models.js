52062368d1c9ec2b2fd811e665bdbc7965e113b6
Carlos Monge <cmonge@jackdevelopers.com>
2020-12-29 22:34:44 -0600

    Add pos update

--- skipMH_module/static/src/js/models.js
+++ skipMH_module/static/src/js/models.js
@@ -3,6 +3,15 @@ odoo.define('skipMH_module.models', function (require) {
 
     var models = require('point_of_sale.models');
 
+    var pos_super = models.PosModel.prototype;
+    models.PosModel = models.PosModel.extend({
+        initialize: function (attributes, options) {
+            pos_super.initialize.apply(this, arguments);
+            console.log(self.partners);
+            return this
+        }
+    });
+
     models.load_fields('res.company', ['street', 'city', 'state_id', 'zip']);
     models.load_fields('res.partner', ['identification_id','skipMH'])
 
