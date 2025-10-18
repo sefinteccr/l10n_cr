24a57ca9309a0ac2daf618a8fbf732f44f3e7291
Carlos Monge <cmonge@jackdevelopers.com>
2025-10-17 19:58:56 -0600

    [IMP] 4.4 resolution update

--- cr_electronic_invoice/__manifest__.py
+++ cr_electronic_invoice/__manifest__.py
@@ -26,7 +26,7 @@
         'data/discount_code_data.xml',
         'data/account_tax_data.xml',
         #'data/aut_ex_data.xml',
-        'data/code_type_product_data.xml',
+        #'data/code_type_product_data.xml',
         'data/identification_type_data.xml',
         'data/ir_cron_data.xml',
         'data/mail_template_data.xml',
